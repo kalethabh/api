817655b21537e52fdb915fbe02a99209421976f2
kalethabh <kalethabh@gmail.com>
2023-08-14 23:34:45 -0500

    api

--- src/db.js
+++ src/db.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgresql://postgres:tgQCc56SCu4OKlWanNLw@containers-us-west-149.railway.app:5475/railway`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -46,10 +47,6 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon, Type } = sequelize.models;
-
-Pokemon.belongsToMany(Type, { through: "pokemons_type" });
-Type.belongsToMany(Pokemon, { through: "pokemons_type" });
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+};
